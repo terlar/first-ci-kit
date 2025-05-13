dacb5aa1222bf50e6afd2a6b132338554e8402ac
Terje Larsen <terlar@gmail.com>
2025-05-13 14:18:34 +0200

    feat: create pipeline packages for both github-actions and gitlab-ci

--- flake-module.nix
+++ flake-module.nix
@@ -10,8 +10,15 @@ in
     description = "Pipelines for CI.";
   };
 
-  config.perSystem.legacyPackages = lib.mapAttrs' (name: value: {
-    name = "ci-pipeline-github-actions-${name}";
-    value = value.pipeline.github-actions.file;
-  }) config.first-ci-kit.pipelines;
+  config.perSystem.legacyPackages = lib.mkMerge [
+    (lib.mapAttrs' (name: value: {
+      name = "ci-pipeline-github-actions-${name}";
+      value = value.pipeline.github-actions.file;
+    }) config.first-ci-kit.pipelines)
+
+    (lib.mapAttrs' (name: value: {
+      name = "ci-pipeline-gitlab-ci-${name}";
+      value = value.pipeline.gitlab-ci.file;
+    }) config.first-ci-kit.pipelines)
+  ];
 }
