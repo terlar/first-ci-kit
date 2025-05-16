c7a6b17a6d790b5aedaa8d6b6046a7bc8132ef51
Terje Larsen <terlar@gmail.com>
2025-05-16 23:40:48 +0200

    chore: explicit process-compose to template

--- template/dual-project/dev/flake.nix
+++ template/dual-project/dev/flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     dev-flake.url = "github:terlar/dev-flake";
     first-ci-kit.url = "github:terlar/first-ci-kit";
-    process-compose.follows = "first-ci-kit/process-compose";
+    process-compose.url = "github:platonic-systems/process-compose-flake";
   };
 
   outputs = _: { };
