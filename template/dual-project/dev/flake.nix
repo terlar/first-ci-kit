4bbc0177fbd23e7e864200e3ff41d7866b36a79f
Terje Larsen <terlar@gmail.com>
2025-05-17 02:00:22 +0200

    chore(template): move nixpkgs to subflake

--- template/dual-project/dev/flake.nix
+++ template/dual-project/dev/flake.nix
@@ -3,8 +3,13 @@
 
   inputs = {
     dev-flake.url = "github:terlar/dev-flake";
-    first-ci-kit.url = "github:terlar/first-ci-kit";
+    first-ci-kit = {
+      url = "github:terlar/first-ci-kit";
+      inputs.flake-parts.follows = "dev-flake/flake-parts";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     process-compose.url = "github:platonic-systems/process-compose-flake";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs = _: { };
--- template/dual-project/flake.nix
+++ template/dual-project/flake.nix
@@ -1,20 +1,12 @@
 {
   description = "My Project";
 
-  inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  };
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
 
   outputs =
     inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "aarch64-darwin"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "x86_64-linux"
-      ];
+      systems = [ ];
 
       imports = [ inputs.flake-parts.flakeModules.partitions ];
 
