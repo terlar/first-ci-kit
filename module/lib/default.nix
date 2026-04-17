3db9d913a79d27427dfa272973c962603970e1e5
Terje Larsen <terlar@gmail.com>
2026-04-17 11:17:26 +0200

    refactor: extract backend-specific dispatch logic to separate files
    Move mkGhaDispatch and mkGitlabDispatchJobs from module/pipelines/default.nix
    into module/pipelines/github-actions.nix and module/pipelines/gitlab-ci.nix
    respectively. Extract shared helpers (expandPipelineNeed, expandPipelineNeeds,
    resolveImage) into ci-lib for reuse. Reduces default.nix to a pure imports file.

--- module/lib/default.nix
+++ module/lib/default.nix
@@ -2,6 +2,20 @@
 
 let
   inherit (lib) types;
+
+  expandPipelineNeed =
+    jobSets: need:
+    if need.jobSet != null then
+      map (job: {
+        inherit job;
+        inherit (need) optional artifacts;
+      }) jobSets.${need.jobSet}.jobs
+    else
+      [
+        {
+          inherit (need) job optional artifacts;
+        }
+      ];
 in
 {
   documentsToYAML = lib.concatMapStringsSep "---\n" (x: (builtins.toJSON x) + "\n");
@@ -44,6 +58,21 @@ in
     artifacts = false;
     optional = true;
   };
+
+  # Expand a single pipeline need given a jobSets attrset.
+  # Returns a list of { job, optional, artifacts }.
+  inherit expandPipelineNeed;
+
+  # Expand a list of pipeline needs into a flat list of { job, optional, artifacts }.
+  expandPipelineNeeds = jobSets: needs: lib.flatten (map (expandPipelineNeed jobSets) needs);
+
+  # Resolve an image from a list of tags via an imageRegistry attrset.
+  resolveImage =
+    tags: imageRegistry:
+    let
+      matchingTag = lib.findFirst (t: imageRegistry ? ${t}) null tags;
+    in
+    if matchingTag != null then imageRegistry.${matchingTag} else null;
 
   replaceVariables =
     variables:
