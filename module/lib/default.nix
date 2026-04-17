0e444a842f85c26bfe7000d5bd139a5cf636bd66
Terje Larsen <terlar@gmail.com>
2026-04-17 08:56:02 +0200

    feat: add pipelines option with inputs and outputs submodule types (#28)
    Introduces the pipelines option (lazyAttrsOf submodule) on each pipeline
    instance. Each entry is an independently-evaluated child pipeline. Inputs
    and outputs are typed submodules matching the GHA workflow_call spec.

--- module/lib/default.nix
+++ module/lib/default.nix
@@ -1,7 +1,43 @@
 { lib, ... }:
 
+let
+  inherit (lib) types;
+in
 {
   documentsToYAML = lib.concatMapStringsSep "---\n" (x: (builtins.toJSON x) + "\n");
+
+  types.needsType = types.submoduleWith {
+    description = "Needs configuration";
+    modules = [
+      {
+        options = {
+          job = lib.mkOption {
+            type = types.nullOr types.str;
+            default = null;
+            description = "Name of the needed job.";
+          };
+
+          jobSet = lib.mkOption {
+            type = types.nullOr types.str;
+            default = null;
+            description = "Name of the needed job set.";
+          };
+
+          optional = lib.mkOption {
+            type = types.bool;
+            default = false;
+            description = "Whether need is optional.";
+          };
+
+          artifacts = lib.mkOption {
+            type = types.bool;
+            default = true;
+            description = "Whether artifacts from dependency are used.";
+          };
+        };
+      }
+    ];
+  };
 
   jobToNeed = job: {
     inherit job;
