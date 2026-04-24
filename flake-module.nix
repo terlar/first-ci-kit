30a5fa64eeff785335d5827d10c750270260c20d
Terje Larsen <terlar@gmail.com>
2026-04-24 12:52:17 +0200

    refactor(pipelines): remove top-level and gha child per-pipeline legacypackages

--- flake-module.nix
+++ flake-module.nix
@@ -37,30 +37,15 @@ in
       packages.gha-path-changes = pkgs.callPackage ./packages/gha-path-changes { };
 
       legacyPackages = lib.mkMerge [
-        (lib.mapAttrs' (name: value: {
-          name = "ci-pipeline-github-actions-${name}";
-          value = value.github-actions.file;
-        }) config.first-ci-kit.pipelines)
-
-        (lib.mapAttrs' (name: value: {
-          name = "ci-pipeline-gitlab-ci-${name}";
-          value = value.gitlab-ci.file;
-        }) config.first-ci-kit.pipelines)
-
-        # Child pipeline legacyPackages: ci-pipeline-{backend}-{parent}-{child}
+        # Child pipeline legacyPackages for GitLab CI: ci-pipeline-gitlab-ci-{parent}-{child}
+        # Required by mkGitlabDispatchJobs which references these by name in generated nix build commands.
         (lib.mkMerge (
           lib.mapAttrsToList (
             parentName: parentValue:
-            lib.mkMerge [
-              (lib.mapAttrs' (childName: childValue: {
-                name = "ci-pipeline-gitlab-ci-${parentName}-${childName}";
-                value = childValue.gitlab-ci.file;
-              }) parentValue.pipelines)
-              (lib.mapAttrs' (childName: childValue: {
-                name = "ci-pipeline-github-actions-${parentName}-${childName}";
-                value = childValue.github-actions.file;
-              }) parentValue.pipelines)
-            ]
+            lib.mapAttrs' (childName: childValue: {
+              name = "ci-pipeline-gitlab-ci-${parentName}-${childName}";
+              value = childValue.gitlab-ci.file;
+            }) parentValue.pipelines
           ) config.first-ci-kit.pipelines
         ))
 
